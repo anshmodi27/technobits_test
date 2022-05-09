8718f619b07e642f9ea0498523815425c007b87f
Ansh Modi <74901469+anshmodi27@users.noreply.github.com>
2022-05-10 01:34:53 +0530

    Update Mot.js

--- src/Components/js/Mot.js
+++ src/Components/js/Mot.js
@@ -6,7 +6,7 @@ const Mot = () => {
       <div class="container me-auto ms-auto row row-cols-1 row-cols-md-4 g-4 m-5">
         <div class="col">
           <div class="card h-100 border-0 text-center">
-            <img src="/images/img3.jpeg" class="img-thumbnail" alt="..." />
+            <img src="/Users/anshmodi27/Documents/Projects/technobits/public/Images/img3.jpeg" class="img-thumbnail" alt="..." />
             <div class="card-body">
               <h5 class="card-title">James Watt</h5>
               <h6 class="card-title">Web Design</h6>
@@ -15,7 +15,7 @@ const Mot = () => {
         </div>
         <div class="col">
           <div class="card h-100 border-0 text-center">
-            <img src="/images/img3.jpeg" class="img-thumbnail" alt="..." />
+            <img src="/Users/anshmodi27/Documents/Projects/technobits/public/Images/img3.jpeg" class="img-thumbnail" alt="..." />
             <div class="card-body">
               <h5 class="card-title">James Havit</h5>
               <h6 class="card-title">Graphic Design</h6>
@@ -24,7 +24,7 @@ const Mot = () => {
         </div>
         <div class="col">
           <div class="card h-100 border-0 text-center">
-            <img src="/images/img3.jpeg" class="img-thumbnail" alt="..." />
+            <img src="/Users/anshmodi27/Documents/Projects/technobits/public/Images/img3.jpeg" class="img-thumbnail" alt="..." />
             <div class="card-body">
               <h5 class="card-title">Alex Roe</h5>
               <h6 class="card-title">UI Design</h6>
@@ -33,7 +33,7 @@ const Mot = () => {
         </div>
         <div class="col">
           <div class="card h-100 border-0 text-center">
-            <img src="/images/img3.jpeg" class="img-thumbnail" alt="..." />
+            <img src="/Users/anshmodi27/Documents/Projects/technobits/public/Images/img3.jpeg" class="img-thumbnail" alt="..." />
             <div class="card-body">
               <h5 class="card-title">Revelis Alcsra</h5>
               <h6 class="card-title">Copywriting</h6>
